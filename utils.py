e5d49d8c8449cf57c0d66d42c04162363a7612f9
sabar-i <142428254+sabar-i@users.noreply.github.com>
2025-04-10 23:19:33 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,64 +1,28 @@
-import time
+# utils.py
 import numpy as np
 import tensorflow as tf
 import random
-import os
 
 def set_seed_tf(seed):
     random.seed(seed)
     np.random.seed(seed)
-    tf.set_random_seed(seed)
-    os.environ['PYTHONHASHSEED'] = str(seed)
+    tf.random.set_seed(seed)  # TF 2.x
 
+class Timer:
+    def __init__(self, name='timer'):
+        self.name = name
 
-class Timer(object):
-    def __init__(self, name=''):
-        self._name = name
-        self.begin_time = time.time()
-        self.last_time = time.time()
-        self.current_time = time.time()
-        self.stage_time = 0.0
-        self.run_time = 0.0
+    def logging(self, content):
+        print(f"{self.name}: {content}")
 
-    def logging(self, message):
-        current_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-        self.update()
-        message = '' if message is None else message
-        print("{} {} {:.0f}s {:.0f}s | {}".format(current_time,
-                                                  self._name,
-                                                  self.run_time,
-                                                  self.stage_time,
-                                                  message))
-
-    def update(self):
-        self.current_time = time.time()
-
-        self.stage_time = self.current_time - self.last_time
-        self.last_time = self.current_time
-        self.run_time = self.current_time - self.begin_time
-        return self
-
-
-def bpr_neg_samp(uni_users, n_users, support_dict, item_array):
-    """
-    param:
-        uni_users - unique users in training data
-        dict - {uid: array[items]}
-        n_users - sample n users
-        neg_num - n of sample pairs for a user.
-        item_array - sample item in this array.
-    return:
-        ret_array - [uid pos_iid neg_iid] * n_records
-    """
-    pos_items = []
-    users = np.random.choice(uni_users, size=n_users, replace=True)
-    for user in users:
-        pos_candidates = support_dict[user]
-        pos_item = random.choice(pos_candidates)
-        pos_items.append(pos_item)
-
-    pos_items = np.array(pos_items, dtype=np.int64).flatten()
-    neg_items = np.random.choice(item_array, len(users), replace=True)
-    ret = np.stack([users, pos_items, neg_items], axis=1)
-    return ret
-
+def bpr_neg_samp(warm_user, n_samp, user_nb_dict, warm_item):
+    pos_item = []
+    neg_item = []
+    warm_item = set(warm_item)
+    for u in warm_user:
+        pos_nb = user_nb_dict[u]
+        pos_item.extend(pos_nb)
+        neg_nb = np.random.choice(list(warm_item - set(pos_nb)), len(pos_nb), replace=False)
+        neg_item.extend(neg_nb)
+    user_batch = np.repeat(warm_user, [len(user_nb_dict[u]) for u in warm_user])
+    return np.stack([user_batch, pos_item, neg_item], axis=1)
